f961573c423ff518569f760da3be5a1b5d208c9b
DongKon Lee <dle0129@gmail.com>
2021-03-02 22:02:26 +0900

    Controller Abstraction

--- src/main/java/com/example/springboot_practice/controller/api/ItemApiController.java
+++ src/main/java/com/example/springboot_practice/controller/api/ItemApiController.java
@@ -1,5 +1,6 @@
 package com.example.springboot_practice.controller.api;
 
+import com.example.springboot_practice.controller.CrudController;
 import com.example.springboot_practice.ifs.CrudInterface;
 import com.example.springboot_practice.model.network.Header;
 import com.example.springboot_practice.model.network.request.ItemApiRequest;
@@ -9,36 +10,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @Slf4j // log
 @RestController
 @RequestMapping("/api/item")
-public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+public class ItemApiController extends CrudController<ItemApiRequest, ItemApiResponse> {
 
     @Autowired
     private ItemApiLogicService itemApiLogicService;
 
-    @Override
-    @PostMapping("") // api/item
-    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.create(request);
+    @PostConstruct
+    public void init() {
+        this.baseService = itemApiLogicService;
     }
 
-    @Override
-    @GetMapping("{id}") // api/item/{id}
-    public Header<ItemApiResponse> read(@PathVariable(name = "id") Long id) {
-        log.info("read id : {}", id);
-        return itemApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("") // api/item
-    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}") // api/item/{id}
-    public Header delete(@PathVariable Long id) {
-        return itemApiLogicService.delete(id);
-    }
 }
