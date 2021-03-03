2afaa5118acd2e3f84ecd4b445fe14bba7c0082a
DongKon Lee <dle0129@gmail.com>
2021-03-03 12:09:06 +0900

    Lombok

--- information_management/src/main/java/com/information/management/demo/domain/Person.java
+++ information_management/src/main/java/com/information/management/demo/domain/Person.java
@@ -1,42 +1,39 @@
 package com.information.management.demo.domain;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class Person {
 
     @Id
     @GeneratedValue // 자동으로 생성되게 끔
     private Long id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private int age;
 
-    public String getName() {
-        return name;
-    }
+    private String hobby;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    private String bloodType;
 
-    public int getAge() {
-        return age;
-    }
+    private String address;
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+    private LocalDate birthday;
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
+    private String job;
+
+    @ToString.Exclude
+    private String phoneNumber;
 }
--- information_management/src/test/java/com/information/management/demo/repository/PersonRepositoryTest.java
+++ information_management/src/test/java/com/information/management/demo/repository/PersonRepositoryTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,5 +34,21 @@ class PersonRepositoryTest {
         Assertions.assertThat(people.size()).isEqualTo(1);
         Assertions.assertThat(people.get(0).getName()).isEqualTo("martin");
         Assertions.assertThat(people.get(0).getAge()).isEqualTo(10);
+    }
+
+    @Test
+    void hashCodeAndEquals() {
+        Person person1 = new Person("martin", 10);
+        Person person2 = new Person("martin", 10);
+
+        System.out.println(person1.equals(person2));
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
+
+        Map<Person, Integer> map = new HashMap<>();
+        map.put(person1, person1.getAge());
+
+        System.out.println(map);
+        System.out.println(map.get(person2));
     }
 }
