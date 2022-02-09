bc86b8460a0942e09ec61e1e88be92a1bfa26099
DongKonLee <dle0129@gmail.com>
2022-02-09 23:13:09 +0900

    로그 추적기 - 파라미터 동기화

---
+++ project/src/main/java/advanced/project/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,41 @@
+package advanced.project.app.v2;
+
+import advanced.project.trace.TraceId;
+import advanced.project.trace.TraceStatus;
+import advanced.project.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV2 {
+    private final HelloTraceV2 trace;
+
+    public void save(TraceId traceId, String itemID) {
+        TraceStatus status = null;
+
+        try {
+            status = trace.beginSync(traceId, "OrderRepository.save()");
+
+            if (itemID.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+
+            throw e;
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
