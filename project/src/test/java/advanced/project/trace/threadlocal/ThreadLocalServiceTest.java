1adbe26028357b7c703f9725efaf86e3eb2890ec
DongKonLee <dle0129@gmail.com>
2022-02-12 23:24:13 +0900

    로그 추적기 - ThreadLocal

--- project/src/test/java/advanced/project/trace/threadlocal/ThreadLocalServiceTest.java
+++ project/src/test/java/advanced/project/trace/threadlocal/ThreadLocalServiceTest.java
@@ -1,21 +1,22 @@
 package advanced.project.trace.threadlocal;
 
 import advanced.project.trace.threadlocal.code.FieldService;
+import advanced.project.trace.threadlocal.code.ThreadLocalService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-public class FieldServiceTest {
+public class ThreadLocalServiceTest {
 
-    private FieldService fieldService = new FieldService();
+    private ThreadLocalService service = new ThreadLocalService();
 
     @Test
     void field() {
         log.info("main start");
 
-        Runnable userA = () -> fieldService.logic("userA");
+        Runnable userA = () -> service.logic("userA");
 
-        Runnable userB = () -> fieldService.logic("userB");
+        Runnable userB = () -> service.logic("userB");
 
         Thread threadA = new Thread(userA);
         threadA.setName("thread-A");
@@ -24,7 +25,7 @@ public class FieldServiceTest {
         threadB.setName("thread-B");
 
         threadA.start();
-        sleep(2000);
+//        sleep(2000);
         threadB.start();
         sleep(2000);
     }
