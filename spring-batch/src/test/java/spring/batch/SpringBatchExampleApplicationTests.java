812ee2c2f7ae476c99c26cda9d1fc94d28808728
DongKonLee <dle0129@gmail.com>
2022-03-01 22:29:23 +0900

    initial commit

--- spring-batch/src/test/java/spring/batch/SpringBatchExampleApplicationTests.java
+++ spring-batch/src/test/java/spring/batch/SpringBatchExampleApplicationTests.java
@@ -1,10 +1,10 @@
-package spring.batch.springbatch;
+package spring.batch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBatchApplicationTests {
+class SpringBatchExampleApplicationTests {
 
     @Test
     void contextLoads() {
