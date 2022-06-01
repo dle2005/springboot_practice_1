ac3bf7834db362b9984dbefb16559aaee4586b83
DongKonLee <dle0129@gmail.com>
2022-06-01 23:49:10 +0900

    주문 도메인 개발
    - 주문, 주문상품 엔티티 개발
    - 주문 리포지토리 개발
    - 주문 서비스 개발
    - 주문 기능 테스트
    - 주문 검색 기능 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,12 +1,16 @@
 package jpabook.jpashop.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
     private String city;
     private String street;
